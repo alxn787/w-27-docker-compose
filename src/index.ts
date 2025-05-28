5df12b14881ca85f56621d4c47a080f795011249
alxn787 <alenabraham787@gmail.com>
2025-05-28 12:21:59 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { PrismaClient } from "../prisma/generated/prisma";
+
+const app = express();
+
+const prisma = new PrismaClient();
+
+app.get("/", async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.json(users);
+});
+
+app.post ("/", async (req,res)=>{
+    const email = Math.random().toString(36).substring(7) + "@example.com"
+    const password = "mysecretpassword"
+    await prisma.user.create({
+        data: {
+            email,
+            password
+        }
+    })
+    res.json({
+        email,
+        password
+    })
+
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
